be59ffb6cfc9f7fda6045af8f2246cce70b555ec
sharad8823 <inominds121@gmail.com>
2025-06-27 16:23:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Load Lottie animations
+lottie.loadAnimation({
+  container: document.getElementById('heart'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/lottie/heart.json'
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('flower'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/lottie/flower.json'
+});
